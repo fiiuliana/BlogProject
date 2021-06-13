fbb62e5792156cd3975e93d6c788f6af7934d1c1
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 22:50:42 +0300

    Added Claims to startup

--- BlogProject.Web/Startup.cs
+++ BlogProject.Web/Startup.cs
@@ -1,13 +1,19 @@
+using BlogProject.Identity;
+using BlogProject.Models.Account;
 using BlogProject.Models.Settings;
 using BlogProject.Repository;
 using BlogProject.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +25,7 @@ namespace BlogProject.Web
 
         public Startup(IConfiguration configuration)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             Configuration = configuration;
         }
                 
@@ -27,6 +34,7 @@ namespace BlogProject.Web
         //connect the model with the Cloudinary options
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
 
             services.AddScoped<ITokenService, TokenService>();
@@ -38,7 +46,43 @@ namespace BlogProject.Web
             services.AddScoped<IAccountRepository, AccountRepository>();
             services.AddScoped<IPhotoRepository, PhotoRepository>();
 
-            // services.AddControllersWithViews();
+            //connected to identity - requires a controller now
+            services.AddIdentityCore<ApplicationUserIdentity>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddUserStore<UserStore>()
+                .AddDefaultTokenProviders()
+                .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+            services.AddControllers();
+            services.AddCors();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer
+            (
+                options =>
+              {
+                  options.RequireHttpsMetadata = false;
+                  options.SaveToken = true;
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidateAudience = true,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+                      ValidIssuer = Configuration["Jwt:Issuer"],
+                      ValidAudience = Configuration["Jwt:Issuer"],
+                      IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                      ClockSkew = TimeSpan.Zero
+                  };
+              }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
