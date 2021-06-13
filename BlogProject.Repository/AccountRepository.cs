996c04d744dc26f79b65722cef9fa17c2737f85d
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 18:24:00 +0300

    Added Blog Repository

---
+++ BlogProject.Repository/AccountRepository.cs
@@ -0,0 +1,79 @@
+﻿using BlogProject.Models.Account;
+using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        //installed package
+        private readonly IConfiguration _config;
+
+        public AccountRepository(IConfiguration config) // will access the appsettings.json
+        { 
+            _config = config;   
+        }
+
+        //Create a virtual table
+        public async Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Username", typeof(string));
+            dataTable.Columns.Add("NormalizedUsername", typeof(string));
+            dataTable.Columns.Add("Email", typeof(string));
+            dataTable.Columns.Add("NormalizedEmail", typeof(string));
+            dataTable.Columns.Add("Fullname", typeof(string));
+            dataTable.Columns.Add("PasswordHash", typeof(string));
+
+            dataTable.Rows.Add(
+                user.Username,
+                user.NormalizedUsername,
+                user.Email,
+                user.NormalizedEmail,
+                user.Fullname,
+                user.PasswordHash
+                );
+            //open a connection with SqlServet
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"))) 
+            {
+                //cancel if something goes wrong
+                await connection.OpenAsync(cancellationToken);
+
+                //insert if all ok
+                await connection.ExecuteAsync("Account_Insert",
+                    new { Account = dataTable.AsTableValuedParameter("dbo.AccountType") },
+                    commandType: CommandType.StoredProcedure);
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<ApplicationUserIdentity> GetByUsernameAsynk(string normalizedUsername, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            ApplicationUserIdentity applicationUser;
+
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                //cancel if something goes wrong
+                await connection.OpenAsync(cancellationToken);
+
+                applicationUser = await connection.QuerySingleOrDefaultAsync<ApplicationUserIdentity>(
+                    "Account_GetByUsername", new { NormalizedUsername = normalizedUsername },
+                    commandType: CommandType.StoredProcedure
+                    );             
+            }
+            return applicationUser;
+        }
+    }
+}
---
+++ BlogProject.Repository/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlogProject.Models.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repository
+{
+    public interface IAccountRepository
+    {
+        //the result is going to be a "promise" - asynk
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsynk(string normalizedUsername,
+            CancellationToken cancellationToken);  //just for the account repository - given
+                                                   //if the registration process stops
+    }
+}
