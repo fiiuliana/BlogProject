436319e2df073132edab9ba7e1cca86fa94c9c20
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 23:25:59 +0300

    Adding first controller - Account Controller Already existing Home - left it there for now

---
+++ BlogProject.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using BlogProject.Models.Account;
+using BlogProject.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Web.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUserIdentity> _userManager;
+        private readonly SignInManager<ApplicationUserIdentity> signInManager;
+
+
+
+    }
+}
