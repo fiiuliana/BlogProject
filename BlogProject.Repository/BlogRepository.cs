d3657c1d31a33701aa9df438b858e7ca56500931
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 19:10:33 +0300

    Added IBlog Repository Interface Started impementation in Blog Repository

---
+++ BlogProject.Repository/BlogRepository.cs
@@ -0,0 +1,42 @@
+﻿using BlogProject.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repository
+{
+    public class BlogRepository : IBlogRepository
+    {
+        public Task<int> DeleteAsync(int blogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResults<Blog>> getAllAsync(BlogPaging blogPaging)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Blog>> GetAllByUserIdAsync(int applicationUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Blog>> GetAllFamousAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Blog> GetAsync(int blogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Blog> UpsertAsync(BlogCreate blogCreate, int applicationUserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlogProject.Repository/IBlogRepository.cs
@@ -0,0 +1,22 @@
+﻿using BlogProject.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repository
+{
+    public interface IBlogRepository
+    {
+        public Task<Blog> UpsertAsync(BlogCreate blogCreate, int applicationUserId);
+        public Task<PagedResults<Blog>> getAllAsync(BlogPaging blogPaging);
+
+        public Task<Blog> GetAsync(int blogId);
+        public Task<List<Blog>> GetAllByUserIdAsync(int applicationUserId);
+        public Task<List<Blog>> GetAllFamousAsync();
+        public Task<int> DeleteAsync(int blogId);
+
+
+    }
+}
