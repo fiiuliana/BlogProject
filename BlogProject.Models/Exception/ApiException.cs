4b993f195e27f39351d3ea413e4871ca51dc108f
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 16:26:44 +0300

    Added Exception model

---
+++ BlogProject.Models/Exception/ApiException.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models.Exception
+{
+    public class ApiException
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);  //take this object and turn it into a Json
+        }
+    }
+}
