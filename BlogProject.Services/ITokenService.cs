92ed4a29ef2b88b77af47ca4ada7a6fafa927cf8
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 22:17:13 +0300

    Added Token service and the method;

---
+++ BlogProject.Services/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using BlogProject.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
