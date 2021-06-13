cf3c796a64b9188b4619227bfb1dfdd661a20712
fiiuliana <fiiulianaf@gmail.com>
2021-06-13 18:33:07 +0300

    Add Interface photo repository Start implementing meth

---
+++ BlogProject.Repository/IPhotoRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlogProject.Models.Photo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repository
+{
+    public interface IPhotoRepository
+    {
+        public Task<Photo> InsertAsynk(PhotoCreate photoCreate, int applicationUserId);
+        public Task<Photo> GetAsynk(int photoId);
+        public Task<Photo> GetAllBtUserIdAsynk(int applicationUserId);
+        public Task<Photo> DeleteAsynk(int photoId);
+
+    }
+}
---
+++ BlogProject.Repository/PhotoRepository.cs
@@ -0,0 +1,32 @@
+﻿using BlogProject.Models.Photo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repository
+{
+    public class PhotoRepository : IPhotoRepository
+    {
+        public Task<Photo> DeleteAsynk(int photoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Photo> GetAllBtUserIdAsynk(int applicationUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Photo> GetAsynk(int photoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Photo> InsertAsynk(PhotoCreate photoCreate, int applicationUserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
