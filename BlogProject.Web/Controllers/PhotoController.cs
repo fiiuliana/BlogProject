6bee580aa661cdb5fdf47a040c57bd0f0544b658
fiiuliana <fiiulianaf@gmail.com>
2021-06-14 18:29:08 +0300

    Completed Photo controller

---
+++ BlogProject.Web/Controllers/PhotoController.cs
@@ -0,0 +1,113 @@
+﻿using BlogProject.Models.Photo;
+using BlogProject.Repository;
+using BlogProject.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IPhotoRepository _photoRepository;
+        private readonly IBlogRepository _blogRepository;
+        private readonly IPhotoService _photoService;
+
+        public PhotoController(
+            IPhotoRepository photoRepository,
+            IBlogRepository blogRepository,
+            IPhotoService photoService)
+        {
+            _photoRepository = photoRepository;
+            _blogRepository = blogRepository;
+            _photoService = photoService;
+        }
+        //start creating endpoints
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Photo>> UploadPhoto(IFormFile file)
+        {
+            // retrieve the user id from token
+            int applicationUserId = int.Parse(User.Claims.First(
+                i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var uploadResult = await _photoService.AddPhotoAsync(file);
+
+            if (uploadResult.Error != null) return BadRequest(uploadResult.Error.Message);
+            // if all good save the reference of the photo
+
+            var photoCreate = new PhotoCreate
+            {
+                PublicId = uploadResult.PublicId,
+                ImageUrl = uploadResult.SecureUrl.AbsoluteUri,
+                Description = file.FileName
+            };
+            // save user id from token in the database alongside the photo pointer
+            var photo = await _photoRepository.InsertAsync(photoCreate, applicationUserId);
+
+            return Ok(photo);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<List<Photo>>> GetByApplicationUserId()
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var photos = await _photoRepository.GetAllBtUserIdAsync(applicationUserId);
+
+            return Ok(photos);
+        }
+
+        [HttpGet("{photoId}")]
+        public async Task<ActionResult<Photo>> Get(int photoId)
+        {
+            var photo = await _photoRepository.GetAsync(photoId);
+
+            return Ok(photo);
+        }
+
+        [Authorize]
+        [HttpDelete("{photoId}")]
+        public async Task<ActionResult<int>> Delete(int photoId)
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var foundPhoto = await _photoRepository.GetAsync(photoId);
+
+            if (foundPhoto != null)
+            {   // delete the photo that belongs to the user
+                if (foundPhoto.ApplicationUserId == applicationUserId)
+                {
+                    // get the blogs of the current user
+                    var blogs = await _blogRepository.GetAllByUserIdAsync(applicationUserId);
+
+                    var usedInBlog = blogs.Any(b => b.PhotoId == photoId);
+
+                    if (usedInBlog) return BadRequest("Cannot remove: photo being used in current blog(s)");
+
+                    var deleteResult = await _photoService.DeletePhotoAsync(foundPhoto.PublicId);
+
+                    if (deleteResult.Error != null) return BadRequest(deleteResult.Error.Message);
+
+                    // if all good delete from database
+                    var affectRows = await _photoRepository.DeleteAsync(foundPhoto.PhotoId);
+                    return Ok(affectRows);
+                }
+                else
+                {
+                    return BadRequest("Photo was not uploaded by the current user");
+                }
+            }
+            return BadRequest("Photo does not exist");
+        }
+    } 
+}
