b565178a41f1052e65cf62d4daca94919c70aef2
fiiuliana <fiiulianaf@gmail.com>
2021-06-15 17:45:28 +0300

    Added the login form (bootstrap) and validation for input fields

--- BlogProject-UI/src/app/app.module.ts
+++ BlogProject-UI/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
@@ -52,6 +53,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
--- BlogProject-UI/src/app/components/login/login.component.ts
+++ BlogProject-UI/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApplicationUserLogin } from 'src/app/models/account/application-user-login.model';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
 
+  constructor(
+
+    private accountService: AccountService,
+    private router: Router,
+    // in order to make reactive and template form validation
+    private formBuilder: FormBuilder
+  ) { 
+    // if the user is logged in - redirect to the dashboard
+    if (this.accountService.isLoggedIn()) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+  // hook into a lifecicle of a specific component
   ngOnInit(): void {
+       this.loginForm = this.formBuilder.group({  //a username filed wich is null and has some validators
+      username: [null, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]],
+      password: [null, [  //has initially a null password and has validators
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(50)
+      ]]
+    });
+  }
+  // is the form touched or not
+  isTouched(field: string){
+    return this.loginForm.get(field).touched;
+  }
+  // are there errors in the form - calculates if there is an error or not
+  hasErrors(field: string) {
+    return this.loginForm.get(field).errors;
+  }
+
+  // there is a specific error
+  hasError(field: string, error: string) {
+    return !!this.loginForm.get(field).hasError(error);
+  }
+
+  onSubmit() {
+    //create another instance and pass in the info from the form
+    let applicationUserLogin: ApplicationUserLogin = new ApplicationUserLogin(
+      this.loginForm.get("username").value,
+      this.loginForm.get("password").value
+    );
+
+    this.accountService.login(applicationUserLogin).subscribe(() => {
+      // an observable is reurned -> can use subscribe -> navigate to dashboard
+      this.router.navigate(['/dashboard']);
+    });
   }
 
 }
