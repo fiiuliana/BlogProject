db6192140631a25da3543e6ff8ffc9ffdd380b93
fiiuliana <fiiulianaf@gmail.com>
2021-06-15 19:11:57 +0300

    correct account model

--- BlogProject.Models/Account/ApplicationUser.cs
+++ BlogProject.Models/Account/ApplicationUser.cs
@@ -9,7 +9,7 @@ namespace BlogProject.Models.Account
     public class ApplicationUser
     {
         public int ApplicationUserId { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Fulname{ get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
