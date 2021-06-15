57eede4149cc9ef96624eacf4fe74cc3d26b791f
fiiuliana <fiiulianaf@gmail.com>
2021-06-15 16:08:21 +0300

    Completed Jwt interceptor

--- BlogProject-UI/src/app/interceptors/jwt.interceptor.ts
+++ BlogProject-UI/src/app/interceptors/jwt.interceptor.ts
@@ -6,13 +6,33 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AccountService } from '../services/account.service';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private accountService: AccountService
+  ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const currentUser = this.accountService.currentUserValue;
+
+    //if there is an user and a token the user is logged in
+    const isLoggedIn = currentUser && currentUser.token;
+    const isApiUrl = request.url.startsWith(environment.webApi);
+    
+    // the bearer is added globally
+    if (isLoggedIn && isApiUrl){
+        request = request.clone({
+          setHeaders:{
+            Authorization: `bearer ${currentUser.token}`
+          }})
+    }
+
     return next.handle(request);
   }
 }
