4f6276d39ccec08cf42a94a250f00f17b7cfbf2a
fiiuliana <fiiulianaf@gmail.com>
2021-06-29 23:42:25 +0300

    Adding the comments

--- BlogProject-UI/src/app/components/comment-components/comment-system/comment-system.component.ts
+++ BlogProject-UI/src/app/components/comment-components/comment-system/comment-system.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BlogCommentViewModel } from 'src/app/models/blog-comment/blog-comment-view-model.model';
+import { BlogComment } from 'src/app/models/blog-comment/blog-comment.model';
+import { AccountService } from 'src/app/services/account.service';
+import { BlogCommentService } from 'src/app/services/blog-comment.service';
 
 @Component({
   selector: 'app-comment-system',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentSystemComponent implements OnInit {
 
-  constructor() { }
+  @Input() blogId: number;
+
+  standAloneComment: BlogCommentViewModel;
+  blogComments: BlogComment[];
+  blogCommentViewModels: BlogCommentViewModel[];
+
+  constructor(
+    private blogCommentService: BlogCommentService,
+    public accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.blogCommentService.getAll(this.blogId).subscribe(blogComments => {
+
+      if (this.accountService.isLoggedIn()) {
+        this.initComment(this.accountService.currentUserValue.username);
+      }
+
+      this.blogComments = blogComments;
+      this.blogCommentViewModels = [];
+
+      for (let i=0; i<this.blogComments.length; i++) {
+        if (!this.blogComments[i].parentBlogCommentId) {
+          this.findCommentReplies(this.blogCommentViewModels, i);
+        }
+      }
+
+    });
+  }
+
+  findCommentReplies(blogCommentViewModels: BlogCommentViewModel[], i: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  
+  initComment(username: string) {
+    throw new Error('Method not implemented.');
   }
 
 }
