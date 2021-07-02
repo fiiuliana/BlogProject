92396bf1a27252b4da9a44b6d8c92667abc4eae4
fiiuliana <fiiulianaf@gmail.com>
2021-07-02 21:39:35 +0300

    Checking the Identity project and update comments

--- BlogProject.Identity/UserStore.cs
+++ BlogProject.Identity/UserStore.cs
@@ -9,26 +9,33 @@ namespace BlogProject.Identity
 {
 
     public class UserStore :
+
+        // needed to implement interfaces in order to use .net core Identity
         IUserStore<ApplicationUserIdentity>,
         IUserEmailStore<ApplicationUserIdentity>,
         IUserPasswordStore<ApplicationUserIdentity>
+
+        // tell the Identity how to: 
     {
 
         private readonly IAccountRepository _accountRepository;
 
+
         public UserStore(IAccountRepository accountRepository) {
             _accountRepository = accountRepository;
         }
+        
         // implemented
         public async Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
-            return await _accountRepository.CreateAsync(user, cancellationToken);
+            return await _accountRepository.CreateAsync(user, cancellationToken); // created in repository
 
         }
+
         // implemented
         public async Task<ApplicationUserIdentity> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
         {
-            return await _accountRepository.GetByUsernameAsync(normalizedUserName, cancellationToken);
+            return await _accountRepository.GetByUsernameAsync(normalizedUserName, cancellationToken);  // creeated in repository
         }
 
 
@@ -37,6 +44,7 @@ namespace BlogProject.Identity
         {
             throw new NotImplementedException();
         }
+
         // left as it is
         public Task<ApplicationUserIdentity> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
         {
@@ -52,18 +60,21 @@ namespace BlogProject.Identity
         // implemented
         public Task<string> GetEmailAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
-            return Task.FromResult(user.Email);
+            return Task.FromResult(user.Email);   // return back the user email
         }
+
         // implemented
         public Task<bool> GetEmailConfirmedAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
-            return Task.FromResult(true);
+            return Task.FromResult(true);    // do not really need it
         }
+
         // implemented
         public Task<string> GetNormalizedEmailAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
-            return Task.FromResult(user.NormalizedEmail);
+            return Task.FromResult(user.NormalizedEmail);   
         }
+
         // implemented
         public Task<string> GetNormalizedUserNameAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
@@ -74,57 +85,67 @@ namespace BlogProject.Identity
         public Task<string> GetPasswordHashAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
         return Task.FromResult(user.PasswordHash);
-    }
+        }
+
         // implemented
         public Task<string> GetUserIdAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
             return Task.FromResult(user.ApplicationUserId.ToString());
         }
+
         // implemented
         public Task<string> GetUserNameAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
             return Task.FromResult(user.Username);
         }
+
         //implemented
         public Task<bool> HasPasswordAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
-            return Task.FromResult(user.PasswordHash != null);
+            return Task.FromResult(user.PasswordHash != null);  // user has a password if it is not null
         }
+
         //implemented
         public Task SetEmailAsync(ApplicationUserIdentity user, string email, CancellationToken cancellationToken)
         {
             user.Email = email;
             return Task.FromResult(0);
         }
+
         //implemented
         public Task SetEmailConfirmedAsync(ApplicationUserIdentity user, bool confirmed, CancellationToken cancellationToken)
         {
             return Task.FromResult(0);
         }
+
         //implemented
         public Task SetNormalizedEmailAsync(ApplicationUserIdentity user, string normalizedEmail, CancellationToken cancellationToken)
         {
             user.NormalizedEmail = normalizedEmail;
             return Task.FromResult(0);
         }
+
         //implemented
         public Task SetNormalizedUserNameAsync(ApplicationUserIdentity user, string normalizedName, CancellationToken cancellationToken)
         {
             user.NormalizedUsername = normalizedName;
             return Task.FromResult(0);
         }
+
         //implemented
         public Task SetPasswordHashAsync(ApplicationUserIdentity user, string passwordHash, CancellationToken cancellationToken)
         {
-            user.PasswordHash = passwordHash;
+            user.PasswordHash = passwordHash; // the password is hashed and returned back - don't need to know how it is hashed
             return Task.FromResult(0);
         }
-        //implemneted
+
+        //implemented
         public Task SetUserNameAsync(ApplicationUserIdentity user, string userName, CancellationToken cancellationToken)
         {
             user.Username = userName;
             return Task.FromResult(0);
         }
+
         //not going to use - not implemented
         public Task<IdentityResult> UpdateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
         {
